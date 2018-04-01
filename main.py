09995ab1a86a3952c5fad23e6077507fb98f9d33
spamdaemon <spamdaemon@comcast.net>
2018-04-01 10:57:54 -0400

    Modified to download the current day's news into the current folder.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+#!/usr/bin/python3
 import requests
 import sys
 import json
 import codecs
 import os
+import re
 from bs4 import BeautifulSoup
+from datetime import date
 
 def main():
 
@@ -14,11 +17,53 @@ def main():
 
 def parse(o):
     for k, v in o[0].items():
-        parseDate(v)
+        if k == date.today().strftime("%Y-%m-%d"):
+            parseDate(k, v)
 
-def parseDate(date):
-    for v in date:
-        parseNews(v)
+def parseDate(date, news):
+    output = date + '.html'
+    folder = 'data/' + date
+
+ #   if os.path.isdir(folder) == False:
+ #       os.makedirs(folder)
+ #       print("Directory \"" + folder + "/\" created")
+ #   elif os.listdir(folder):
+ #       print("Directory \"" + folder + "/\" exists!\n\nAbort.\n")
+ #       return
+
+    items = []
+    content = []
+    for i in news:
+        item = parseNews(i);
+        items.append(item)
+        content.append(item["content"])
+
+    with open(output, "w") as f:
+        print('<?xml version="1.0" encoding="UTF-8" ?>', file=f)
+        print("<!DOCTYPE html>",file=f)
+        print("<html lang='ja'>",file=f)
+        print('<head><meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" >',file=f)
+#        print('<style type="text/css">body { margin-left: 1em; margin-right: 1em; writing-mode:tb-rl; -epub-writing-mode: vertical-rl; -webkit-writing-mode: vertical-rl; line-break: normal; -epub-line-break: normal; -webkit-line-break: normal;} {font-family: serif;} p { text-indent: 1em;} h2{ font-size: large; font-weight: bold;}</style>',file=f)
+#        print('<style type="text/css">body { margin-left: 1em; margin-right: 1em; line-break: normal; -epub-line-break: normal; -webkit-line-break: normal;} {font-family: serif;} p { text-indent: 1em;} h2{ font-size: large; font-weight: bold;}</style>',file=f)
+        print('<style type="text/css">body { margin-left: 1em; margin-right: 1em; line-break: normal; -webkit-line-break: normal;} {font-family: serif;} p { font-size: large; text-indent: 1em;} h2{ font-size: x-large; font-weight: bold;}</style>',file=f)
+        print("</head>",file=f)
+        print("<body>",file=f)
+        print("<br />".join(content),file=f)
+        print("</body>",file=f)
+        print("</html>",file=f)
+        print("File \"" + output + "\" created")
+        
+       
+    for i in items:
+        if not "voice" in i or not "uri" in i["voice"]:
+            continue
+
+        r = requests.get(i["voice"]["uri"])
+        with open(folder + '/' + i["voice"]["file"], "wb") as f:
+            f.write(r.content)
+            print("File \"" + i["voice"]["file"] + "\" downloaded")
+
+    print("Today news were downloaded from NHK.")
 
 def parseNews(news):
     news_id = news['news_id']
@@ -27,40 +72,36 @@ def parseNews(news):
     title_ruby = news['title_with_ruby']
     news_uri = 'http://www3.nhk.or.jp/news/easy/' + str(news_id) + '/' + str(news_id) + '.html'
 
-    news_folder = 'data/' + news_time + '_' + news_id + '_' + title
-    news_folder = news_folder.replace(' ', '_')
-    news_file = str(news_id) + '.html'
+    r = requests.get(news_uri)
+    r.encoding = 'utf-8'
 
-    if os.path.isdir(news_folder) == False:
-        os.makedirs(news_folder)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    date = soup.find('p', attrs={'id':'newsDate'}).contents[0]
+    title = soup.find('div', attrs={'id':'newstitle'})#.find('h2')
+    article = soup.find('div', attrs={'id':'newsarticle'})
 
-        r = requests.get(news_uri)
-        r.encoding = 'utf-8'
+    for a in article.findAll('a'):
+        a.unwrap()
 
-        soup = BeautifulSoup(r.text, 'html.parser')
-        date = soup.find('p', attrs={'id':'newsDate'}).contents[0]
-        title = soup.find('div', attrs={'id':'newstitle'})#.find('h2')
-        article = soup.find('div', attrs={'id':'newsarticle'})
+ #   if news['has_news_easy_voice'] == True:
+ #       voice_file = re.sub(r"[^\da-z\.]i", "", news['news_easy_voice_uri'])
+ #       voice = {
+ #           "uri": 'http://www3.nhk.or.jp/news/easy/' + str(news_id) + '/' + str(voice_file),
+ #           "file": voice_file
+ #       }
+ #       # add audio tag to mp3 file
+ #       link = """<audio preload="metadata" controls="controls">
+ #               <source src="{src}" type="audio/mpeg; codecs=mp3" />
+ #               <a href="{src}" target="_blank">Download voice file</a>
+ #               </audio>""".replace("{src}", voice_file)
+ #       article.append(BeautifulSoup(link, 'html.parser'))
+ #
+ #   else:
+        voice = {}
 
-        for a in article.findAll('a'):
-            a.unwrap()
-
-        with open(news_folder + '/' + news_file, "w") as f:
-            print("<!DOCTYPE html>", file=f)
-            print("<html lang='ja'>", file=f)
-            print("<head><meta charset='utf-8'></head>", file=f)
-            print("<style>p { font-size: 100%; line-height: 3.2; padding-bottom: 20px; }</style>", file=f)
-            print("<body>", file=f)
-            print(title, file=f)
-            print(article, file=f)
-            print("</body>", file=f)
-            print("</html>", file=f)
-
-        if news['has_news_easy_voice'] == True:
-            voice_file = news['news_easy_voice_uri']
-            voice_uri = 'http://www3.nhk.or.jp/news/easy/' + str(news_id) + '/' + str(voice_file)
-            r = requests.get(voice_uri)
-            with open(news_folder + '/' + voice_file, "wb") as f:
-                f.write(r.content)
+    return {
+        "content": str(title) + str(article),
+        "voice": voice
+    }
 
 main()
